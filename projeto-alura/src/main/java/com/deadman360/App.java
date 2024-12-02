4e977b947567531ba424cd0efa771a0762f7c669
deadman <vmartinho.kfg@gmail.com>
2024-12-02 13:00:15 -0300

    projeto

--- projeto-alura/src/main/java/com/deadman360/App.java
+++ projeto-alura/src/main/java/com/deadman360/App.java
@@ -4,10 +4,11 @@ package com.deadman360;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        Filme topgun = new Filme("Top Gun", 2020, true, 8.3, 5435, 190);
+        topgun.setIncluido(false);
+        topgun.setNome("Top gun: Maverick");
     }
 }
