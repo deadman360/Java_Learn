bed7a22ba46ec95ac6f1c8e9cd07a6c402e1733b
deadman <vmartinho.kfg@gmail.com>
2024-12-02 22:46:19 -0300

    curso finalizado

--- projeto-alura/src/main/java/com/deadman360/App.java
+++ projeto-alura/src/main/java/com/deadman360/App.java
@@ -1,6 +1,7 @@
 package com.deadman360;
 
 import com.deadman360.projeto.caclulos.CalculadoraDeTempo;
+import com.deadman360.projeto.caclulos.Recomendacao;
 import com.deadman360.projeto.modelos.Filme;
 import com.deadman360.projeto.modelos.Serie;
 
@@ -13,15 +14,15 @@ public class App {
     public static void main(String[] args) {
         // Titulo topgun = new Titulo("Top Gun", 2020, true, 190);
         // topgun.exibeFichaTencica();
-        // topgun.avalia(8.39);
-        // topgun.avalia(5.65);
-        // topgun.avalia(5.98);
-        // topgun.avalia(10);
         // System.out.println("Media: %.2f".formatted(topgun.getMedia()));
         Filme topgun = new Filme();
         topgun.setDuracao(190);
         topgun.setNome("TopGun: Maverick");
         topgun.setAnoLancamento(2020);
+        topgun.avalia(8.39);
+        topgun.avalia(5.65);
+        topgun.avalia(5.98);
+        topgun.avalia(10);
 
         Serie supernatural = new Serie();
         supernatural.setNome("Supernatural");
@@ -33,9 +34,16 @@ public class App {
         CalculadoraDeTempo calc = new CalculadoraDeTempo();
         calc.incluiTempo(topgun);
         calc.incluiTempo(supernatural);
-        System.out.println("Tempo em minutos de sua lista: %d".formatted(calc.getTempoTotal()));
-        System.out.println("Tempo em horas de sua lista: %d".formatted(calc.getHoras()));
-        System.out.println("Tempo em dias de sua lista: %.2f".formatted(calc.getDias()));
-
+        System.out.println(
+            "Tempo em minutos de sua lista: %d".formatted(calc.getTempoTotal())
+        );
+        System.out.println(
+            "Tempo em horas de sua lista: %d".formatted(calc.getHoras())
+        );
+        System.out.println(
+            "Tempo em dias de sua lista: %.2f".formatted(calc.getDias())
+        );
+        Recomendacao filtro = new Recomendacao();
+        filtro.filtra(topgun);
     }
 }
--- projeto-alura/src/main/java/com/deadman360/projeto/modelos/Filme.java
+++ projeto-alura/src/main/java/com/deadman360/projeto/modelos/Filme.java
@@ -3,19 +3,19 @@ package com.deadman360.projeto.modelos;
 import com.deadman360.projeto.caclulos.Classificavel;
 
 public class Filme extends Titulo implements Classificavel {
+
     private String diretor;
 
     public String getDiretor() {
         return diretor;
     }
+
     public void setDiretor(String diretor) {
         this.diretor = diretor;
     }
 
     @Override
     public int getClassificacao() {
-        return (int) this.getMedia()/2;
+        return (int) (this.getMedia() / 2);
     }
-
-    
 }
